dc042746e05e5130c3427d8c412363190abc8a0a
zhengzhijie <zhengzhijie@renrendai.com>
2016-03-24 18:26:07 +0800

    check if exists before drop table

--- tables.my.sql
+++ tables.my.sql
@@ -16,7 +16,7 @@
 # (3) If you add new fields then you should also change the global variable
 #     $standard_fields.   Note that if you are just adding custom fields for
 #     a single site then this is not necessary.
-DROP TABLE mrbs_area;
+DROP TABLE if exists mrbs_area;
 CREATE TABLE mrbs_area
 (
   id                        int NOT NULL auto_increment,
@@ -59,7 +59,7 @@ CREATE TABLE mrbs_area
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_room;
+DROP TABLE if exists mrbs_room;
 CREATE TABLE mrbs_room
 (
   id               int NOT NULL auto_increment,
@@ -76,7 +76,7 @@ CREATE TABLE mrbs_room
   KEY idxSortKey (sort_key)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_entry;
+DROP TABLE if exists mrbs_entry;
 CREATE TABLE mrbs_entry
 (
   id             int NOT NULL auto_increment,
@@ -107,7 +107,7 @@ CREATE TABLE mrbs_entry
   KEY idxEndTime   (end_time)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_repeat;
+DROP TABLE if exists mrbs_repeat;
 CREATE TABLE mrbs_repeat
 (
   id             int NOT NULL auto_increment,
@@ -139,7 +139,7 @@ CREATE TABLE mrbs_repeat
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_variables;
+DROP TABLE if exists mrbs_variables;
 CREATE TABLE mrbs_variables
 (
   id               int NOT NULL auto_increment,
@@ -149,7 +149,7 @@ CREATE TABLE mrbs_variables
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_zoneinfo;
+DROP TABLE if exists mrbs_zoneinfo;
 CREATE TABLE mrbs_zoneinfo
 (
   id                 int NOT NULL auto_increment,
@@ -161,7 +161,7 @@ CREATE TABLE mrbs_zoneinfo
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE mrbs_users;
+DROP TABLE if exists mrbs_users;
 CREATE TABLE mrbs_users
 (
   /* The first four fields are required. Don't remove. */
